d43c25215af87255a73bb2b72a435fac1ad3382b
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2020-09-30 07:18:28 +0700

    Initial commit

---
+++ CNPM_MyPham/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CNPM_MyPham.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
